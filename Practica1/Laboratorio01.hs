9dc04524d75aeadc425cbb43b211d40417c20ddf
Yahir León <yahir.leon@ciencias.unam.mx>
2026-08-19 14:55:15 -0600

    Ejercicios 1,2 y 3

--- Practica1/Laboratorio01.hs
+++ Practica1/Laboratorio01.hs
@@ -1,10 +1,13 @@
 module Laboratorio01 where
 
 distanciaOrigen :: Double -> Double -> Double
+distanciaOrigen x y = sqrt((x)^2 + (y)^2)
 
 sumaCuadradosPares :: [Int] -> Int
+sumaCuadradosPares xs = sum (map (^2) (filter even xs))
 
 aplicaTresVeces :: (a -> a) -> a -> a
+aplicaTresVeces f x = f (f (f x))
 
 varianza2 :: Double -> Double -> Double
 
